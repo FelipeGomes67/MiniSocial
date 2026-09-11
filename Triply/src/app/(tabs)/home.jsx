7a9214cff4c3e2fa12ab485aa925fd0aaed01a42
FELIPE TOROLHO <42400772819@info.local>
2026-09-11 10:32:54 -0300

    atualizada a home

--- Triply/src/app/(tabs)/home.jsx
+++ Triply/src/app/(tabs)/home.jsx
@@ -1,6 +1,8 @@
-import { Text, View, TouchableOpacity, homeStyleheet, Image, SafeAreaView, ScrollView } from "react-native";
+import { Text, View, TouchableOpacity, homeStyleheet, Image, ScrollView } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { homeStyle } from "../../styles/homeStyle";
+import { SafeAreaView } from "react-native-safe-area-context";
+
 
 
 export default function Home() {
@@ -18,33 +20,33 @@ export default function Home() {
         <View style={homeStyle.cardPublicacao}>
           <View style={homeStyle.linhaPerfil}>
             <View style={homeStyle.cardPerfil}>
-              <Text style={homeStyle.cardPerfilText}></Text>
+              <Text style={[homeStyle.cardPerfilText, homeStyle.fonte]}>P</Text>
             </View>
 
             <View style={homeStyle.infoPerfil}>
-              <Text style={homeStyle.nomePerfil}>Pessoa1</Text>
-              <Text style={homeStyle.nomePerfilData}>01/01/2024</Text>
+              <Text style={[homeStyle.nomePerfil, homeStyle.fonte]}>Pessoa1</Text>
+              <Text style={[homeStyle.nomePerfilData, homeStyle.fonte]}>01/01/2024</Text>
             </View>
           </View>
 
-          <Text style={homeStyle.descricaoPublicacao}>
+          <Text style={[homeStyle.descricaoPublicacao, homeStyle.fonte]}>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
           </Text>
 
           <View style={homeStyle.botaoCurtir}>
             <TouchableOpacity style={homeStyle.botaoCurtir}>
               <Ionicons name="heart" size={24} color="#000000" />
-              <Text>100</Text>
+              <Text style={[homeStyle.fonte]}>100</Text>
             </TouchableOpacity>
 
             <TouchableOpacity style={homeStyle.botaoComentar}>
               <Ionicons name="chatbubble" size={24} color="#000000" />
-              <Text>33</Text>
+              <Text style={[homeStyle.fonte]}>33</Text>
             </TouchableOpacity>
 
             <TouchableOpacity style={homeStyle.botaoSalvar}>
               <Ionicons name="bookmark-outline" size={24} color="#000000" />
-              <Text>33</Text>
+              <Text style={[homeStyle.fonte]}>33</Text>
             </TouchableOpacity>
           </View>
         </View>
@@ -52,16 +54,16 @@ export default function Home() {
         <View style={homeStyle.cardPublicacao}>
           <View style={homeStyle.linhaPerfil}>
             <View style={homeStyle.cardPerfil}>
-              <Text style={homeStyle.cardPerfilText}></Text>
+              <Text style={[homeStyle.cardPerfilText, homeStyle.fonte]}>P</Text>
             </View>
 
             <View style={homeStyle.infoPerfil}>
-              <Text style={homeStyle.nomePerfil}>Pessoa1</Text>
-              <Text style={homeStyle.nomePerfilData}>01/01/2024</Text>
+              <Text style={[homeStyle.nomePerfil, homeStyle.fonte]}>Pessoa1</Text>
+              <Text style={[homeStyle.nomePerfilData, homeStyle.fonte]}>01/01/2024</Text>
             </View>
           </View>
 
-          <Text style={homeStyle.descricaoPublicacao}>
+          <Text style={[homeStyle.descricaoPublicacao, homeStyle.fonte]}>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
           </Text>
 
@@ -70,17 +72,17 @@ export default function Home() {
           <View style={homeStyle.botaoCurtir}>
             <TouchableOpacity style={homeStyle.botaoCurtir}>
               <Ionicons name="heart" size={24} color="#000000" />
-              <Text>100</Text>
+              <Text style={[homeStyle.fonte]}>100</Text>
             </TouchableOpacity>
 
             <TouchableOpacity style={homeStyle.botaoComentar}>
               <Ionicons name="chatbubble" size={24} color="#000000" />
-              <Text>33</Text>
+              <Text style={[homeStyle.fonte]}>33</Text>
             </TouchableOpacity>
 
             <TouchableOpacity style={homeStyle.botaoSalvar}>
               <Ionicons name="bookmark-outline" size={24} color="#000000" />
-              <Text>33</Text>
+              <Text style={[homeStyle.fonte]}>33</Text>
             </TouchableOpacity>
           </View>
         </View>
